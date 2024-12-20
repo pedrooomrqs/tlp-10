b8cd7f32872cfacb089e601571c6abbc4c15ecf8
mrqs <146396924+pedrooomrqs@users.noreply.github.com>
2024-12-20 20:42:26 +0000

    Update Encontro.cpp

--- Modulo 3/Encontro.cpp
+++ Modulo 3/Encontro.cpp
@@ -8,7 +8,9 @@ void main()
 {
 	srand(time(0));
 	setlocale(LC_ALL, "Portuguese");
-	string huzz[5] = { "Melody", "Inês", "Daniela", "Marta", "Clara" },
+	string 
+		huzz[5] = { "Melody", "InÃªs", "Daniela", "Marta", "Clara" },
 		lugares[5] = { "Bowling", "Bar", "Praia", "Escola", "Restaurante" };
+	
 	cout << "A " << huzz[rand() % 5] << " vai te encontrar na/no " << lugares[rand() % 5];
 }
